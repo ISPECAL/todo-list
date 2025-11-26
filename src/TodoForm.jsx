6dc8fe580c444d6a8b4cb3ea602fee820add51f5
Ilce Perez <ilcesperezc@gmail.com>
2025-11-25 20:59:55 -0800

    form added

---
+++ src/TodoForm.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+function TodoForm(){
+return(
+    <form>
+        <label htmlFor='TodoTitle'>Todo</label>
+        <input type= "text" id="TodoTitle"/>
+        <button type="button">Add todo</button>
+
+
+
+    </form>
+
+);
+
+}
+
+export default TodoForm;
